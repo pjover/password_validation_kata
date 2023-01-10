a48f02f17eb4992181c008ad0b652362d9d05daa
Pere Jover <pjover@gmail.com>
2023-01-10 18:45:11 +0100

    Contains a capital letter

--- internal/password_validation.go
+++ internal/password_validation.go
@@ -1,5 +1,28 @@
 package internal
 
+import (
+	"unicode"
+)
+
 func ValidatePassword(password string) bool {
-	return true
+	if len(password) < 8 {
+		return false
+	}
+	if check_all_chars(password) {
+		return true
+	}
+	return false
+}
+
+func check_all_chars(password string) bool {
+	for i := 0; i < len(password); i++ {
+		if check_capital(rune(password[i])) {
+			return true
+		}
+	}
+	return false
+}
+
+func check_capital(letter rune) bool {
+	return unicode.IsUpper(letter)
 }
--- internal/password_validation_test.go
+++ internal/password_validation_test.go
@@ -12,9 +12,34 @@ func TestValidatePassword(t *testing.T) {
 		want     bool
 	}{
 		{
-			name:     "Starting",
-			password: "bla",
+			name:     "8 characters",
+			password: "Ab345678",
 			want:     true,
+		},
+		{
+			name:     "7 characters",
+			password: "1234",
+			want:     false,
+		},
+		{
+			name:     "With capital letter",
+			password: "Test_capital",
+			want:     true,
+		},
+		{
+			name:     "Without capital letter",
+			password: "test_capital",
+			want:     false,
+		},
+		{
+			name:     "With lowercase letter",
+			password: "Test_capital",
+			want:     true,
+		},
+		{
+			name:     "Without lowercase letter",
+			password: "AAAAAAAAA",
+			want:     false,
 		},
 	}
 	for _, tt := range tests {
